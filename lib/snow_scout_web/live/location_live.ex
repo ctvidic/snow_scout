f866acad6e019bff7b9d47b9121eeeef7cade257
Christopher Vidic <chrisvidic.cv@gmail.com>
2022-12-07 23:40:02 -0700

    accomodate 4 integer station_names

--- lib/snow_scout_web/live/location_live.ex
+++ lib/snow_scout_web/live/location_live.ex
@@ -14,20 +14,27 @@ defmodule SnowScoutWeb.LocationsLive do
   end
 
   def parse_triplet(triplet) do
-    "https://wcc.sc.egov.usda.gov/nwcc/site?sitenum="<>String.slice(triplet, 1..3)
+    length = String.length(triplet)
+    if length == 13 do
+      "https://wcc.sc.egov.usda.gov/nwcc/site?sitenum="<>String.slice(triplet, 1..3)
+    else
+      "https://wcc.sc.egov.usda.gov/nwcc/site?sitenum="<>String.slice(triplet, 1..4)
+    end
   end
 
   def render(assigns) do
     ~H"""
-      <%= for station <- @stations do %>
-      <div class="weather-card">
-        <li>Station Name: <%= inspect station.name %></li>
-        <li>Lat: <%= inspect station.latitude %> Lng: <%= inspect station.longitude %></li>
-        <li><%= inspect station.triplet %></li>
-        <li><a href={parse_triplet(station.triplet)} target="_blank">Snotel Link </a></li>
-        <li><%= link "Station Page", to: Routes.solo_path(@socket, :show, station.triplet)%></li>
+      <div class="flex flex-col items-center w-1000 !important">
+        <%= for station <- @stations do %>
+        <div class="weather-card w-1000">
+          <li>Station Name: <%= inspect station.name %></li>
+          <li>Lat: <%= inspect station.latitude %> Lng: <%= inspect station.longitude %></li>
+          <li><%= inspect station.triplet %></li>
+          <li><a href={parse_triplet(station.triplet)} target="_blank">Snotel Link </a></li>
+          <li><%= link "Station Page", to: Routes.solo_path(@socket, :show, station.triplet)%></li>
+        </div>
+      <% end %>
       </div>
-    <% end %>
     """
   end
 end
